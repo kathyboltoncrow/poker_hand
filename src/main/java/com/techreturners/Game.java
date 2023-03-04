df3e728be7ac9dbddf8e8b160a7a63733e5f2fec
kathyboltoncrow <kathy.bolton.crow@gmail.com>
2023-03-04 11:59:38 +0000

    consistent with using ArrayList

--- src/main/java/com/techreturners/Game.java
+++ src/main/java/com/techreturners/Game.java
@@ -3,50 +3,47 @@ package com.techreturners;
 import com.techreturners.enums.Rank;
 import com.techreturners.enums.Suit;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Game {
 
-    private static Card [] deck;
     private static final int DECK_CARDS = 52;
     private static final int NUMBER_CARDS_PER_HAND = 5;
 
-    public Card [] initialiseDeck() {
-        deck = new Card[DECK_CARDS];
-        int index = 0;
+    private List<Card> deck;
 
-        for(int i = 0; i < 13; i++){
+
+    public List<Card> initialiseDeck() {
+        deck = new ArrayList<>(DECK_CARDS);
+
+        for (int i = 0; i < 13; i++) {
             Rank value = Rank.values()[i];
-            for(int j = 0; j < 4; j++){
-                deck[index] = new Card(value, Suit.values()[j]);
-             //   System.out.println(value.toString() + " of " + Suit.values()[j].toString());
-                index++;
+            for (int j = 0; j < 4; j++) {
+                Card card = new Card(value, Suit.values()[j]);
+                deck.add(card);
             }
         }
-
         return deck;
-
     }
 
-    public Card[] deal() {
+        public List<Card> deal() {
+            ArrayList<Card> hand = new ArrayList<Card>();
+            ArrayList<Card> shuffledDeck = new ArrayList<Card>(deck);
+            Collections.shuffle(shuffledDeck);
 
-        Card [] hand = new Card[NUMBER_CARDS_PER_HAND];
+            for(int i = 0; i < NUMBER_CARDS_PER_HAND; i++) {
+               hand.add(shuffledDeck.get(i));
+            }
 
-        List<Card> shuffledDeck = Arrays.asList(deck);
-        Collections.shuffle(shuffledDeck);
+            return hand;
 
-        for(int i = 0; i < NUMBER_CARDS_PER_HAND; i++) {
-            hand[i] = shuffledDeck.get(i);
-        }
-       // System.out.println(Arrays.stream(hand).toList().toString());
-        return hand;
-    }
-
-
-
-
-
+         }
 
 }
+
+
+
+
+
+
+
--- src/test/java/com/techreturners/GameTest.java
+++ src/test/java/com/techreturners/GameTest.java
@@ -3,20 +3,22 @@ package com.techreturners;
 import com.techreturners.enums.Suit;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
     @Test
     public void initialiseDeck(){
     Game game = new Game();
-    Card [] deck = game.initialiseDeck();
-    assertEquals(52,deck.length);
+    List<Card> deck = game.initialiseDeck();
+    assertEquals(52,deck.size());
     }
     @Test
     public void initialiseHand(){
         Game game = new Game();
-        Card [] deck = game.initialiseDeck();
-        Card [] hand = game.deal();
-        assertEquals(5,hand.length);
+        List<Card> deck = game.initialiseDeck();
+        List<Card> hand = game.deal();
+        assertEquals(5,hand.size());
     }
 }
